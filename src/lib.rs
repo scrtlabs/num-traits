3ff6271922718c63510f84a8eca6f9dbcf635a02
Assaf Morami <assaf.morami@gmail.com>
2023-03-22 23:34:29 +0200

    Disable floats by default

--- src/lib.rs
+++ src/lib.rs
@@ -18,34 +18,37 @@
 extern crate num_traits;
 
 pub use bounds::Bounded;
+#[cfg(feature = "floats")]
 pub use float::{Float, FloatConst};
 // pub use real::Real; // NOTE: Don't do this, it breaks `use num_traits::*;`.
-pub use identities::{Zero, One, zero, one};
-pub use ops::checked::{CheckedAdd, CheckedSub, CheckedMul, CheckedDiv, CheckedShl, CheckedShr};
-pub use ops::wrapping::{WrappingAdd, WrappingMul, WrappingSub};
-pub use ops::saturating::Saturating;
-pub use sign::{Signed, Unsigned, abs, abs_sub, signum};
-pub use cast::{AsPrimitive, FromPrimitive, ToPrimitive, NumCast, cast};
+pub use cast::{cast, AsPrimitive, FromPrimitive, NumCast, ToPrimitive};
+pub use identities::{one, zero, One, Zero};
 pub use int::PrimInt;
-pub use pow::{pow, checked_pow};
-
+pub use ops::checked::*;
+pub use ops::checked::{CheckedAdd, CheckedDiv, CheckedMul, CheckedShl, CheckedShr, CheckedSub};
+pub use ops::saturating::Saturating;
+pub use ops::wrapping::*;
+pub use ops::wrapping::{WrappingAdd, WrappingMul, WrappingSub};
+pub use pow::{checked_pow, pow};
+pub use sign::{abs, abs_sub, signum, Signed, Unsigned};
 
 // Re-exports from num-traits 0.2!
 
-pub use num_traits::{Num, NumOps, NumRef, RefNum};
-pub use num_traits::{NumAssignOps, NumAssign, NumAssignRef};
-pub use num_traits::{FloatErrorKind, ParseFloatError};
 pub use num_traits::clamp;
+#[cfg(feature = "floats")]
+pub use num_traits::{FloatErrorKind, ParseFloatError};
+pub use num_traits::{Num, NumOps, NumRef, RefNum};
+pub use num_traits::{NumAssign, NumAssignOps, NumAssignRef};
 
 // Note: the module structure is explicitly re-created, rather than re-exporting en masse,
 // so we won't expose any items that may be added later in the new version.
 
 pub mod identities {
-    pub use num_traits::identities::{Zero, One, zero, one};
+    pub use num_traits::identities::{one, zero, One, Zero};
 }
 
 pub mod sign {
-    pub use num_traits::sign::{Signed, Unsigned, abs, abs_sub, signum};
+    pub use num_traits::sign::{abs, abs_sub, signum, Signed, Unsigned};
 }
 
 pub mod ops {
@@ -54,8 +57,9 @@ pub mod ops {
     }
 
     pub mod checked {
-        pub use num_traits::ops::checked::{CheckedAdd, CheckedSub, CheckedMul, CheckedDiv,
-                                           CheckedShl, CheckedShr};
+        pub use num_traits::ops::checked::{
+            CheckedAdd, CheckedDiv, CheckedMul, CheckedShl, CheckedShr, CheckedSub,
+        };
     }
 
     pub mod wrapping {
@@ -67,6 +71,7 @@ pub mod bounds {
     pub use num_traits::bounds::Bounded;
 }
 
+#[cfg(feature = "floats")]
 pub mod float {
     pub use num_traits::float::{Float, FloatConst};
 }
@@ -76,7 +81,7 @@ pub mod real {
 }
 
 pub mod cast {
-    pub use num_traits::cast::{AsPrimitive, FromPrimitive, ToPrimitive, NumCast, cast};
+    pub use num_traits::cast::{cast, AsPrimitive, FromPrimitive, NumCast, ToPrimitive};
 }
 
 pub mod int {
@@ -84,5 +89,5 @@ pub mod int {
 }
 
 pub mod pow {
-    pub use num_traits::pow::{pow, checked_pow};
+    pub use num_traits::pow::{checked_pow, pow};
 }
